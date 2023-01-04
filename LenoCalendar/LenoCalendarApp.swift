5f6678356a829a18252a7303b0664499fe335ad8
彭智鑫 <pengzhixin@live.cn>
2023-01-04 14:46:24 +0800

    Initial Commit

---
+++ LenoCalendar/LenoCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenoCalendarApp.swift
+//  LenoCalendar
+//
+//  Created by 彭智鑫 on 2023/1/4.
+//
+
+import SwiftUI
+
+@main
+struct LenoCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
